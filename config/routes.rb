1cc987d41caafe5ebb65d9de34cde3247d880162
sekitaku <sekitaku56@gmail.com>
2019-04-30 19:38:57 +0900

    ルートパスを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'lunch/go'
+  get 'lunch/go' # => "lunch#top"
+  root 'lunch#go'
 end
