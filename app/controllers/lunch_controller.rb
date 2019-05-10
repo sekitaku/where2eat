936e09cb8cbb13246b3cede7d46791a81f989c9a
sekitaku <sekitaku56@gmail.com>
2019-05-10 15:41:56 +0900

    ランダムに表示

--- app/controllers/lunch_controller.rb
+++ app/controllers/lunch_controller.rb
@@ -1,4 +1,6 @@
 class LunchController < ApplicationController
   def go
+    eataries = ["東松屋", "西松屋", "松のや", "志恩"]
+    @recommend = eataries.sample
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'lunch/go' # => "lunch#top"
+  get 'lunch/go' # => "lunch#go"
   root 'lunch#go'
 end
