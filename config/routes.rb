57b230cdd974618b8cc93cb9f7fd6948189d2a91
sekitaku <sekitaku56@gmail.com>
2019-05-17 17:56:03 +0900

    login session added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'eateries/new'
   post 'eateries/create'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'lunch/go' # => "lunch#go"
   root 'lunch#go'
+  get '/login', to:'sessions#new'
+  post 'sessions/create'
 end
